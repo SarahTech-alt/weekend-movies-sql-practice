3633e4deda68820d4dbb752bf34481a5e2d61fb8
Sarah Fuoss <sfuoss@gmail.com>
2021-10-03 12:01:00 -0500

    Completed base mode.

--- sql-practice-assignment.sql
+++ sql-practice-assignment.sql
@@ -2,11 +2,31 @@
 -- Add the SQL that does what is asked in each question.
 
 -- 1. Select all movies with the 'Adventure' genre? Use the id.
+SELECT "movies"."title", "genres"."name" from "movies"
+JOIN "movies_genres" ON "movies_genres"."movie_id" = "movies"."id"
+JOIN "genres" ON "genres"."id"="movies_genres"."genre_id" WHERE "genres"."name"='Adventure';
 
-INSERT your SQL here :)
+-- Avatar	Adventure
+-- Beauty and the Beast	Adventure
+
 
 -- 2. Get the count of movies that have each genre.  
 -- Don't worry about unused genres (Disaster has 0 movies)
+SELECT "genres"."name", count("movies"."id") from "movies"
+JOIN "movies_genres" ON "movies_genres"."movie_id" = "movies"."id"
+JOIN "genres" ON "genres"."id"="movies_genres"."genre_id" GROUP BY "genres"."name";
+
+Adventure	2
+Musical	2
+Romantic	1
+Epic	1
+Comedy	7
+Animated	2
+Biographical	4
+Fantasy	1
+Drama	2
+Space-Opera	2
+Science Fiction	2
 
 Example Result:
 ---------------------------------
@@ -23,11 +43,12 @@ Example Result:
 
 -- 3. Add the genre "Superhero" to "Star Wars".
 
-
+INSERT INTO "movies_genres" ("movie_id", "genre_id")
+VALUES ('10', '13');
 
 -- 4. Remove the "Comedy" genre from "Titanic"
 
-
+DELETE FROM "movies_genres" WHERE "movie_id"='13' AND "genre_id"='4';
 
 -- Stretch
 
